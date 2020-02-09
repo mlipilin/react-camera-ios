94a4b3c9cc15549ee5a71fd7857eca6fef72807a
Mikhail Lipilin <lipilin.m@gmail.com>
2020-02-09 19:03:39 +0300

    setup gh-pages
    * build:gh-pages npm script
    
    * deploy npm script

---
+++ README.md
@@ -0,0 +1,3 @@
+[![GitHub Actions](https://github.com/mlipilin/react-camera-ios/workflows/Run%20tests/badge.svg)](https://github.com/mlipilin/transfermarkt-parser/actions)
+
+# React Camera iOS
---
+++ config/webpack.gh-pages.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const merge = require('webpack-merge');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const PrerenderSPAPlugin = require('prerender-spa-plugin');
+
+const common = require('./webpack.common');
+
+const pathOutput = path.resolve(__dirname, '../gh-pages');
+const pathIcons = path.resolve(__dirname, '../public/*.+(png|ico)');
+const pathIconsOutput = path.resolve(__dirname, '../gh-pages');
+
+module.exports = merge(common, {
+  mode: 'production',
+  output: {
+    filename: 'script.js',
+    path: pathOutput,
+    publicPath: '',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, '../gh-pages/index.html'),
+      template: path.resolve(__dirname, '../public/index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'styles.css',
+    }),
+    new CopyPlugin([
+      {
+        from: pathIcons,
+        to: pathIconsOutput,
+        flatten: true,
+      },
+    ]),
+    new PrerenderSPAPlugin({
+      staticDir: path.join(__dirname, '../gh-pages'),
+      routes: ['/'],
+    }),
+  ],
+});
