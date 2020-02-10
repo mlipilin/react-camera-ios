407bbf80b65f8d9cad934286096bacf2da482edf
Mikhail Lipilin <lipilin.m@gmail.com>
2020-02-10 18:57:23 +0300

    setup webpack for proper build

--- src/index.js
+++ src/index.js
@@ -1,15 +1,6 @@
 /* eslint-disable react/jsx-filename-extension */
-// import Camera from './components/Camera';
-// import { DEVICE, PLACEMENT, SIDE } from './constants';
+import Camera from './components/Camera';
+import { DEVICE, PLACEMENT, SIDE } from './constants';
 
-// export { DEVICE, PLACEMENT, SIDE };
-// export default Camera;
-
-import React, { useState } from 'react';
-
-export default function RRR() {
-  const [clicks, setClicks] = useState(0);
-  return (
-    <button type="button" onClick={() => setClicks(clicks + 1)}>{clicks}</button>
-  );
-}
+export { DEVICE, PLACEMENT, SIDE };
+export default Camera;
