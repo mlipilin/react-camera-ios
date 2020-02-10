15cff25ece45f2f716c3d137b6670de34e15eb4c
Mikhail Lipilin <lipilin.m@gmail.com>
2020-02-10 17:34:28 +0300

    RRR

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
-import Camera from './components/Camera';
-import { DEVICE, PLACEMENT, SIDE } from './constants';
+/* eslint-disable react/jsx-filename-extension */
+// import Camera from './components/Camera';
+// import { DEVICE, PLACEMENT, SIDE } from './constants';
 
-export { DEVICE, PLACEMENT, SIDE };
-export default Camera;
+// export { DEVICE, PLACEMENT, SIDE };
+// export default Camera;
+
+import React, { useState } from 'react';
+
+export default function RRR() {
+  const [clicks, setClicks] = useState(0);
+  return (
+    <button type="button" onClick={() => setClicks(clicks + 1)}>{clicks}</button>
+  );
+}
